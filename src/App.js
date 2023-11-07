a3f4e5babe0fbbd73a1045a717c4f5e3cfe9a6e4
geek09rk <raghav.kataria09@gmail.com>
2023-11-07 16:27:42 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React  from 'react';
+
+import {HUPOXNET} from './HUPOXNET';
+
+function App() {
+
+  return (
+
+
+<div className="App">
+<HUPOXNET />
+
+  </div>
+  );
+}
+
+export default App;
